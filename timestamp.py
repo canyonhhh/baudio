c3c3e415285382926578262cae47f8f0b1d25840
Joris <jorislisas@gmail.com>
2023-09-20 12:10:29 +0300

    add timestamp to log

--- timestamp.py
+++ timestamp.py
@@ -3,12 +3,17 @@ import cgi
 import json
 import os
 import sys
+from datetime import datetime
 
 timestamp_file = "timestamp.txt"
+log_file = "/var/log/timestamp_changes.log"
 
 def save_timestamp(timestamp):
     with open(timestamp_file, "w") as f:
         f.write(str(timestamp))
+    with open(log_file, "a") as log:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        log.write(f"{current_time} - Timestamp changed to: {timestamp}\n")
 
 def get_timestamp():
     if os.path.exists(timestamp_file):
